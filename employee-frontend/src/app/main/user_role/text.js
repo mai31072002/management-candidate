e5edb87216c9d97d1a97f1152d662bff57df204f
chienon <ongngocchien02@gmail.com>
2026-04-02 16:05:47 +0700

    update test CI/CD
    update test CI/CD

--- employee-frontend/src/app/main/user_role/text.js
+++ employee-frontend/src/app/main/user_role/text.js
@@ -1,41 +1,41 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-function UserList() {
-  const [data, setData] = useState([]);       // lưu dữ liệu
-  const [loading, setLoading] = useState(false); // trạng thái loading
-  const [error, setError] = useState(null);   // lỗi
+// function UserList() {
+//   const [data, setData] = useState([]);       // lưu dữ liệu
+//   const [loading, setLoading] = useState(false); // trạng thái loading
+//   const [error, setError] = useState(null);   // lỗi
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      setLoading(true);
-      setError(null);
+//   useEffect(() => {
+//     const fetchUsers = async () => {
+//       setLoading(true);
+//       setError(null);
 
-      try {
-        const res = await fetch("https://api.com/users");
-        const result = await res.json();
+//       try {
+//         const res = await fetch("https://api.com/users");
+//         const result = await res.json();
 
-        setData(result);
-      } catch (err) {
-        setError(err.message || "Something went wrong");
-      } finally {
-        setLoading(false);
-      }
-    };
+//         setData(result);
+//       } catch (err) {
+//         setError(err.message || "Something went wrong");
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
 
-    fetchUsers();
-  }, []);
+//     fetchUsers();
+//   }, []);
 
-  // render UI
-  if (loading) return <div>Loading...</div>; // hoặc Spin của Antd
-  if (error) return <div>Error: {error}</div>;
+//   // render UI
+//   if (loading) return <div>Loading...</div>; // hoặc Spin của Antd
+//   if (error) return <div>Error: {error}</div>;
 
-  return (
-    <div>
-      {data.map(user => (
-        <div key={user.id}>{user.name}</div>
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {data.map(user => (
+//         <div key={user.id}>{user.name}</div>
+//       ))}
+//     </div>
+//   );
+// }
 
-export default UserList;
+// export default UserList;
